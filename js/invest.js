9f2eef510abfbfffd6ace64e24736dba2d023476
ismart-maker <ismart@usc.edu>
2026-04-29 13:49:15 -0700

    Fixed header and updated invest page per feedback

--- js/invest.js
+++ js/invest.js
@@ -1,20 +1,52 @@
 $(document).ready(function() {
+  $('.invest-why__learn').on('click', function(e) {
+    e.preventDefault();
+
+    const $link = $(this);
+    const $extraInfo = $link.next('.invest-why__extra-info');
+
+    $extraInfo.slideToggle(300, function() {
+      if ($extraInfo.is(':visible')) {
+        $link.text('Show less');
+      } else {
+        $link.text('Learn more');
+      }
+    });
+
+    $('.invest-why__extra-info').not($extraInfo).slideUp(300);
+    $('.invest-why__learn').not($link).text('Learn more');
+  });
+});
+
+$(document).ready(function() {
   $('.dot').on('click', function(e) {
     e.stopPropagation();
 
-    // 1. Grab data from the dot
-    const name = $(this).data('name');
-    const price = $(this).data('price');
+    const $dot = $(this);
     
-    // 2. Update the card content
-    $('#info-card .product-title').text(name);
-    $('#info-card .product-price').text(price);
+    const name = $dot.data('name');
+    const price = $dot.data('price');
+    const imgFile = $dot.data('img');
+    
+    const $card = $('#info-card');
+    const $title = $card.find('.product-title');
+    const $price = $card.find('.product-price');
+    const $imgContainer = $card.find('.product-img'); 
 
-    // 3. Just show it (it will stay where the CSS tells it to stay)
-    $('#info-card').stop(true, true).fadeIn(200);
+    $title.text(name);
+    $price.text(price);
+    
+    $imgContainer.html(`<img src="img/${imgFile}" alt="${name}" style="width:100%; height:auto; display:block;">`);
+
+    const pos = $dot.position(); 
+    
+    $card.css({
+      top: (pos.top + 30) + 'px', 
+      left: (pos.left + 30) + 'px',
+      position: 'absolute'
+    }).stop(true, true).fadeIn(200);
   });
 
-  // Hide card when clicking the background
   $(document).on('click', function() {
     $('#info-card').fadeOut(200);
   });
